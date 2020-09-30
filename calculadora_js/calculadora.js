2a2f14f29bc6a492b08298ca762f0114fce151dc
Vinicius Henrique <Vinicius-almeida01@hotmail.com>
2020-09-30 17:00:26 -0300

    Uma calculadora cujo sua unica funcao é somar, recomendo que utilize a do google pois aquela é a braba

---
+++ calculadora_js/calculadora.js
@@ -0,0 +1,12 @@
+var num1 = prompt("Digite o primeiro numero1: ");
+var num2 = prompt("Digite o primeiro numero2: ");
+
+var n1conver = parseInt(num1);
+var n2conver = parseInt(num2);
+
+var resultado_soma = n1conver + n2conver;
+
+  console.log(resultado_soma);
+
+alert("A soma é de:" + resultado_soma); // lembrar que cocatenação é com +
+										// não sei se era para acontecer algo porque so fica em branco, mesmo usando alert ao inves de prompt
